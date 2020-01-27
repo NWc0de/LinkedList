d350de078f17c426ac936a6d62bcb038cdbee881
Spencer Little <Slittle57@gmail.com>
2020-01-27 15:36:32 -0800

    Initial commit

---
+++ src/Driver.java
@@ -0,0 +1,40 @@
+/*
+ * Reads test input and displays results.
+ * Author: Spencer Little
+ */
+
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Driver {
+
+    //TODO: Allow files to be passed on command line
+    public static void main(String[] args) {
+        LinkedList<Integer> ints = readIntFile("TestData/random100.txt");
+        //LinkedList<Integer> ints = new LinkedList<>(new Integer[] {150,75,43,900,123,532});
+        System.out.println(ints);
+        ints.bubbleSort();
+        System.out.println(ints);
+    }
+
+    private static LinkedList<Integer> readIntFile(String fName) {
+        LinkedList<Integer> fileData = new LinkedList<>();
+        try {
+            Scanner in = new Scanner(new File(fName));
+            while (in.hasNextInt()) fileData.addLast(in.nextInt());
+
+        } catch (FileNotFoundException fne) {
+            System.out.println("Unable to find/open file, is file name correct?");
+            System.exit(1);
+        }
+
+        return fileData;
+    }
+
+    private static String linkedListToString(LinkedList<Integer> in) {
+        return in.toString();
+    }
+
+}
---
+++ src/LinkedList.java
@@ -0,0 +1,145 @@
+/*
+ * Implements a basic linked list.
+ * Author: Spencer Little
+ */
+
+/**
+ * A generic LinkedList and associated sorting methods.
+ * @param <T> the type of element to be stored by the list, must be comparable for sorting
+ */
+public class LinkedList<T extends Comparable> {
+
+    private Node root;
+    private Node tail;
+    private int size;
+
+    LinkedList() { size = 0; }
+    LinkedList(T[] elems) {addAll(elems);}
+    LinkedList(T elem) {
+        root = new Node(elem);
+        tail = root;
+        size = 1;
+    }
+
+    /**************************************************
+     *           Data Access/Manipulation             *
+     **************************************************/
+
+    public int size() {return size;}
+    public T getFirst() {return root.getElemement();}
+    public T getLast() {return tail.getElemement();}
+    public T get(int ind) {
+        if (ind >= size) throw new IllegalArgumentException("Index " + ind + " out of bounds for length " + size());
+        Node n = root;
+        for (int i = 0; i < ind; i++) {
+            n = n.getNext();
+        }
+        return n.getElemement();
+    }
+
+    public void addFirst(T elem) {
+        Node in = new Node(elem);
+        size++;
+        in.setNext(root);
+        root = in;
+    }
+
+    public void addLast(T elem) {
+        Node in = new Node(elem);
+        size++;
+        if (tail == null) root = in;
+        else tail.setNext(in);
+        tail = in;
+    }
+
+    public void addAll(T[] elems) {
+        for (T el : elems) {
+            addLast(el);
+        }
+    }
+
+    /**
+     * Sorts the linked list with bubble sort.
+     */
+    public void bubbleSort() {
+        if (size <= 1) return;
+        for (int i = 0; i < size - 1; i++) {
+            Node prev = null, curr = root;
+            boolean swap = false;
+            for (int j = 0; j < size - 1 - i; j++) {
+                if (curr.compareTo(curr.getNext()) > 0) {
+                    Node tmp = curr.getNext(); // will become new previous
+                    swapNodes(prev, curr, curr, curr.getNext()); // current is implicitly updated
+                    prev = tmp;
+                    swap = true;
+                } else {
+                    prev = curr;
+                    curr = curr.getNext();
+                }
+            }
+            if (!swap) return;
+        }
+    }
+
+
+    /**************************************************
+     *                Auxiliary Methods               *
+     **************************************************/
+
+    /**
+     * Returns a string representation of the linked list.
+     */
+    @Override
+    public String toString() {
+        StringBuilder asString = new StringBuilder();
+        for (Node x = root; x != null; x = x.next) {
+            asString.append("[ " + x.getElemement() + " ] -> ");
+        }
+        return asString.toString();
+    }
+
+    /**
+     * Swaps nodes swap1 and swap2, requires the nodes "behind" (pointing to)
+     * swap1, and swap2: prev1, and prev2 respectively.
+     */
+    private void swapNodes(Node prev1, Node swap1, Node prev2, Node swap2) {
+        boolean invalidNodes = (prev1 != null && prev1.getNext() != swap1)
+                || (prev2 != null && prev2.getNext() != swap2);
+        if (invalidNodes) throw new IllegalArgumentException("Previous nodes do not point to the swap nodes.");
+
+        Node next1 = swap1.getNext(), next2 = swap2.getNext();
+        swap1.setNext(next2);
+        if (next1 == swap2) swap2.setNext(swap1); // assure swap2 doesn't end up pointing to itself
+        else swap2.setNext(next1);
+
+        if (prev1 != null) prev1.setNext(swap2);
+        else root = swap2;
+        if (prev2 != null && prev2 != swap1) prev2.setNext(swap1);
+        else if (prev2 == null) root = swap2;
+    }
+
+    /*
+     * A simple class representing a linked node.
+     */
+    private class Node {
+        private T elem;
+        private Node next;
+
+        Node() {}
+        Node(T elem) {this.elem = elem;}
+
+        public Node getNext() {return next;}
+        public void setNext(Node next) {this.next = next;}
+
+        public T getElemement() {return elem;}
+        public void setElem(T elem) {this.elem = elem;}
+
+        /*
+         * Compares this node with n based on the each nodes element.
+         * Returns a negative int if this < n, 0 if they are equal, or a positive int if this > n
+         */
+        public int compareTo(Node n) {
+            return getElemement().compareTo(n.getElemement());
+        }
+    }
+}
